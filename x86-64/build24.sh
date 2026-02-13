d4f5ccc5192db85f8eae50c566237d281ae4c6cb
Liu Wei <47049957+wuwangchuxinxq@users.noreply.github.com>
2026-02-13 18:10:58 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -52,6 +52,7 @@ PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 #24.10
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # ======== shell/custom-packages.sh =======
